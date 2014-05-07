fd0a909f0a932ae691d1858a4ccf17c31fa4b4f2
Paulo Francisco <paulo.francisco@totvs.com.br>
2014-05-07 14:48:01 -0300

    Melhorando interface

--- js/fluig_extension.js
+++ js/fluig_extension.js
@@ -1,10 +1,38 @@
 var fluigExtension = {
+	
+	serverUrl: "http://10.80.81.96:8080",
+	jProgress: null,
+	jContent: null,
+	
 	init : function() {
+		var self = this;
+		self.jProgress = $('#progress');
+		self.jContent = $('#fluig_content_post');
+		
+		$(document).ajaxStart(function() {
+			self.updateProgress(0, 50, 1000);
+		});
+		
+		$(document).ajaxComplete(function(event,request, settings) {
+			self.updateProgress(50, 105, 1000);
+		});
+		
+		
+		if(self.isLogged() == true){
+			$('#fluig_extension_login').hide();
+			$('#fluig_extensin_content').show();
+		}else{
+			$('#fluig_extensin_content').hide();
+			$('#fluig_extension_login').show();
+			$('#login_fluig').attr('href', self.serverUrl + '/portal');
+		}
+		
 		jQuery.urlShortener.settings.apiKey='AIzaSyD50283xRGCwMj3Qmk_qu19v5FyteAGZ-Q';
 		
 		var currentUrl = null;
 		var currentTitle = null;
 		var contentPost = null;
+		
 		
 		chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
 			currentUrl = tabs[0].url;
@@ -14,29 +42,76 @@ var fluigExtension = {
 				longUrl: currentUrl,
 				success: function (shortUrl) {
 					contentPost = currentTitle + " " + shortUrl;
-					$('#fluig_content_post').html(contentPost);
-				},
-				error: function(err) {
-					$('#fluig_content_post').html("Ops! Não podemos postar esse link! =/");
+					self.jContent.html(contentPost);
 				}
 			});
 		});
 		
+		
 		$('#fluig_publish_button').click(function(){
+			self.jContent.attr('disabled','true');
+			
 	    	$.ajax({
 				type: "POST",
 				async: true,
-				url: "http://10.80.81.96:8080/api/public/social/post/create",
+				url: self.serverUrl + "/api/public/social/post/create",
 				dataType: "json",
 				contentType: "application/json",
 				data: JSON.stringify({text: contentPost}),
 				success: function(data){
-					console.log("feito: " + data);
+					self.postOk();
 				},
 				error: function(data){
-					console.log("Erro: " + data);
+					if(data.status == 200){
+						self.postOk();
+					}
 				}
 			});
+		});
+	},
+	
+	postOk: function(){
+		var jPost = $('#fluig_publish_button');
+		jPost.css('padding-left', '0.8571em');
+		jPost.css('background-image', 'none');
+
+		$("#airplane").animate({
+			top : "-=250",
+			left : "+=100"
+		}, 1500, function() {
+			window.close();
+		});
+		
+	},
+	
+	isLogged: function(){
+		var self = this;
+		var logged = false;
+		
+		$.ajax({
+			type: "GET",
+			async: false,
+			url: self.serverUrl + "/api/public/social/user/current",
+			contentType: "application/json",
+			success: function(data){
+				logged = true;
+			}
+		});
+    	
+    	return logged;
+	},
+	
+	updateProgress: function(startProgress, endProgress, durantion) {
+		var self = this;
+		$({property: startProgress}).animate({property: endProgress}, {
+		    duration: durantion,
+		    step: function() {
+		        var percent = Math.round(this.property);
+		        self.jProgress.css('width', percent + "%");
+		        if(percent == 105) {
+		        	self.jProgress.addClass("done");
+		        }
+		    },
 		});
 	}
 };
