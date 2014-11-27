1d9eef5527119c97d37f56f29841e3d09f034781
Paulo Roberto Francisco Júnior <paulo.francisco@totvs.com.br>
2014-11-27 17:34:54 -0200

    Adicionando tela de salvar servidor

--- js/fluig_extension.js
+++ js/fluig_extension.js
@@ -1,5 +1,5 @@
 var fluigExtension = {
-	
+
 	serverUrl: null,
 	KEY_SERVER_URL: "fluigextension.serverurl",
 
@@ -9,9 +9,9 @@ var fluigExtension = {
 	jPublishButton: null,
 	jStartLoad: null,
 	jTop: null,
-	
+
 	contentPost: "",
-	
+
 	loadJsFiles: function(){
 		var self = this;
 		setTimeout(function(){
@@ -23,34 +23,27 @@ var fluigExtension = {
 			});
 		}, 1000);
 	},
-	
+
 	init : function() {
 		var self = this;
 		self.loadJQueryObjects();
 		self.loadJQueryEvents();
 		self.loadServerUrl();
 		self.loadTop();
-		
-		if(self.serverUrl){
-			self.jTop.removeClass('full-height');
-		}else{
-			self.jTop.addClass('full-height');
-			self.jEditServer.click();
-		}
-		
-		/*var jExtension = $('#fluig_extensin_content');
+
+		var jExtension = $('#fluig_extensin_content');
 		var jLogin = $('#fluig_extension_login');
-		
+
 		if(self.isLogged() == true){
 			jLogin.hide();
 			self.jStartLoad.fadeOut(function(){
 				$('html').css('background-color','#EBEBEB');
 				jExtension.show();
 			});
-			
+
 			var currentUrl = null;
 			var currentTitle = null;
-			
+
 			jQuery.urlShortener.settings.apiKey='AIzaSyD50283xRGCwMj3Qmk_qu19v5FyteAGZ-Q';
 			chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
 				currentUrl = tabs[0].url;
@@ -67,11 +60,11 @@ var fluigExtension = {
 					}
 				});
 			});
-			
+
 			self.jPublishButton.click(function(){
 				self.contentPost = self.jContentPost.val();
 				self.jContentPost.attr('disabled','true');
-				
+
 		    	$.ajax({
 					type: "POST",
 					async: true,
@@ -96,20 +89,29 @@ var fluigExtension = {
 			self.jStartLoad.fadeOut(function(){
 				jLogin.show();
 			});
-		}*/
+		}
 	},
-	
+
 	loadTop: function(){
 		var self = this;
 		self.jStartLoad.fadeOut(function(){
+			self.jStartLoad.remove();
+
+			if(self.serverUrl){
+				self.jTop.removeClass('full-height');
+			}else{
+				self.jTop.addClass('full-height');
+				self.jEditServer.click();
+			}
+
 			self.jTop.fadeIn();
 		});
 	},
-	
+
 	postOk: function(){
 		var jAirplane = $('#airplane');
 		jAirplane.show();
-		
+
 		var self = this;
 		self.jPublishButton.css('padding-left', '0.8571em');
 		self.jPublishButton.css('background-image', 'none');
@@ -121,11 +123,11 @@ var fluigExtension = {
 			window.close();
 		});
 	},
-	
+
 	isLogged: function(){
 		var self = this;
 		var logged = false;
-		
+
 		$.ajax({
 			type: "GET",
 			async: false,
@@ -140,21 +142,26 @@ var fluigExtension = {
 				console.log("invalid session");
 			}
 		});
-    	
+
     	return logged;
 	},
-	
+
+	saveServerUrl: function(){
+		var jServerValInput = $('#serval_val_input');
+		this.setServerUrl(jServerValInput.val());
+	},
+
 	setServerUrl: function(serverUrl){
 		var self = this;
 		self.serverUrl = serverUrl;
 		localStorage.setItem(self.KEY_SERVER_URL, serverUrl);
 	},
-	
+
 	loadServerUrl: function(){
 		var self = this;
 		self.serverUrl = localStorage.getItem(self.KEY_SERVER_URL);
 	},
-	
+
 	loadJsFile: function(filename, callback){
 		var fileref=document.createElement('script');
 		fileref.setAttribute("type","text/javascript");
@@ -162,10 +169,10 @@ var fluigExtension = {
 		fileref.onload = callback;
 		document.getElementsByTagName("head")[0].appendChild(fileref);
 	},
-	
+
 	loadJQueryObjects: function(){
 		var self = this;
-		
+
 		self.jContentPost = $('#fluig_content_post');
 		self.jEditServer = $('#edit_server');
 		self.jProgress = $('#progress');
@@ -173,7 +180,7 @@ var fluigExtension = {
 		self.jStartLoad = $('#fluig_extension_start_load');
 		self.jTop = $('#fluig_extension_top');
 	},
-	
+
 	loadJQueryEvents: function(){
 		var self = this;
 		var jServerVal = $('#server_val');
@@ -182,13 +189,13 @@ var fluigExtension = {
 				$(this).find('span').show();
 			}
 		});
-		
+
 		jServerVal.mouseout(function(){
 			if($(this).hasClass('inactive')){
 				$(this).find('span').hide();
 			}
 		});
-		
+
 		var jServerValInput = $('#serval_val_input');
 		self.jEditServer.click(function(){
 			if(jServerVal.hasClass('inactive')){
@@ -200,8 +207,18 @@ var fluigExtension = {
 			}
 			jServerValInput.show().focus();
 		});
+
+		jServerValInput.on('keyup', function(event){
+			if (event.which == 13) {
+				self.saveServerUrl();
+			}
+		});
+
+		$('#save_server').on('click', function(){
+			self.saveServerUrl();
+		});
 	},
-	
+
 	updateProgress: function(startProgress, endProgress, durantion) {
 		var self = this;
 		$({property: startProgress}).animate({property: endProgress}, {
